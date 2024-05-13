47f720331a5e79dd8b69a141642c2dceb67a46ad
Said Zúñiga <saidalejandro.zg@gmail.com>
2024-05-12 23:48:54 -0600

    ItemList Favorite Toogle
    Toogle button for adding/removing favorite banks

--- app/src/main/java/com/example/appyaganaste/ui/bankList/BankListItem.kt
+++ app/src/main/java/com/example/appyaganaste/ui/bankList/BankListItem.kt
@@ -4,13 +4,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,9 +28,12 @@ import com.example.appyaganaste.ui.theme.AppYaGanasteTheme
 @Composable
 fun BankListItem(
     bank: Bank,
-    onClick: () -> Unit){
+    isFavorite: Boolean,
+    onFavoriteClicked: () -> Unit,
+    onClick: () -> Unit
+) {
     Card(
-        Modifier
+        modifier = Modifier
             .padding(16.dp)
             .fillMaxWidth()
             .height(100.dp),
@@ -62,6 +71,17 @@ fun BankListItem(
                     Text(text = bank.bankName, style = MaterialTheme.typography.titleLarge)
                     Text(text = bank.description, style = MaterialTheme.typography.bodyMedium)
                 }
+                IconButton(
+                    onClick = { onFavoriteClicked() },
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .align(Alignment.CenterVertically)
+                ) {
+                    Icon(
+                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = "Toggle favorite"
+                    )
+                }
             }
         }
     }
@@ -72,6 +92,6 @@ fun BankListItem(
 fun DefaultPreview() {
     AppYaGanasteTheme {
         val bank = Bank("Banco internacional",0,"","Nombre del Banco")
-        BankListItem(bank = bank, onClick = {})
+        BankListItem(bank = bank, onClick = {}, isFavorite = false, onFavoriteClicked = {})
     }
 }
